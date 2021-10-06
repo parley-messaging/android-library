3b9a676acad582f7a3ad7a89ad11653099b5d7cd
Alex Kok <a.kok@webuildapps.com>
2021-10-06 15:35:07 +0200

    Added all the other API versions

--- parley/src/main/java/nu/parley/android/data/model/ApiVersion.java
+++ parley/src/main/java/nu/parley/android/data/model/ApiVersion.java
@@ -2,11 +2,29 @@ package nu.parley.android.data.model;
 
 public enum ApiVersion {
     /**
-     * @deprecated
-     * It is recommended to use {@link ApiVersion#V1_6} to support the latest functionality of Parley.
+     * @deprecated A newer version is available to support the latest functionality of Parley.
      */
-    @Deprecated
-    V1_2,
+    @Deprecated V1_0,
+    /**
+     * @deprecated A newer version is available to support the latest functionality of Parley.
+     */
+    @Deprecated V1_1,
+    /**
+     * @deprecated A newer version is available to support the latest functionality of Parley.
+     */
+    @Deprecated V1_2,
+    /**
+     * @deprecated A newer version is available to support the latest functionality of Parley.
+     */
+    @Deprecated V1_3,
+    /**
+     * @deprecated A newer version is available to support the latest functionality of Parley.
+     */
+    @Deprecated V1_4,
+    /**
+     * @deprecated A newer version is available to support the latest functionality of Parley.
+     */
+    @Deprecated V1_5,
     /**
      * This is the latest supported version by the library.
      */
