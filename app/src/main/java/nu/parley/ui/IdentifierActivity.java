faa1471a3ef5fe4de9b0d527592efcc577e75748
Alex Kok <a.kok@webuildapps.com>
2022-10-18 12:26:20 +0200

    Added support for targetting Android 12

--- app/src/main/java/nu/parley/ui/IdentifierActivity.java
+++ app/src/main/java/nu/parley/ui/IdentifierActivity.java
@@ -15,8 +15,7 @@ import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -153,14 +152,12 @@ public final class IdentifierActivity extends BaseActivity {
     }
 
     private void setFirebaseToken() {
-        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
             @Override
-            public void onComplete(@NonNull Task<InstanceIdResult> task) {
-                String token = null;
+            public void onComplete(@NonNull Task<String> task) {
                 if (task.isSuccessful() && task.getResult() != null) {
-                    token = task.getResult().getToken();
+                    Parley.setPushToken(task.getResult());
                 }
-                Parley.setPushToken(token);
             }
         });
     }
